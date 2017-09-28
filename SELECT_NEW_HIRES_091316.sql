9e6d419ba26fccf144f76532a23e16f38b508a60
HMOTEN <humzamoten@hotmail.com>
2017-09-28 10:26:52 -0500

    Delete SELECT_NEW_HIRES_091316.sql

--- SELECT_NEW_HIRES_091316.sql
+++
@@ -1,7 +0,0 @@
-#SELECTS NEW HIRES that exists within 091316wnulls that didnt exist within 091216wnulls
-#CONTRIBUTERS: UTD Team
-SELECT  *
-FROM    091316wnulls
-WHERE   empID NOT IN (SELECT empid FROM 091216wnulls)
-
-
